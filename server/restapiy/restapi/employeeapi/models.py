5287ea53e9c27b1375db6ca55be8ac719da25def
click2cloud-yaswanth <konatham.kumar@click2cloud.net>
2022-06-03 14:35:16 +0530

    updated functions

--- server/restapiy/restapi/employeeapi/models.py
+++ server/restapiy/restapi/employeeapi/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Employee(models.Model):
     full_name=models.CharField(max_length=100,default="")
-    employee_code = models.CharField(max_length=3,default="")
+    employee_code = models.CharField(max_length=100,default="")
     mobilenum = models.CharField(max_length=15,default="")
 
